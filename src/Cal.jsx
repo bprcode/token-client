605a3eeba722e7b5387e92736dd613d7eef2f910
bprcode <bprcode@gmail.com>
2023-09-03 16:58:40 -0700

    Switch to MUI Table elements

--- src/Cal.jsx
+++ src/Cal.jsx
@@ -12,6 +12,7 @@ import {
   TableRow,
   ThemeProvider,
   Typography,
+  styled,
 } from '@mui/material'
 import { CssBaseline } from '@mui/material'
 import digitalTheme from './blueDigitalTheme'
@@ -24,77 +25,26 @@ const log = console.log.bind(console)
 const currentDate = dayjs()
 
 function Demo() {
+  const [expandedDate, setExpandedDate] = useState(null)
+
   return (
     <Container maxWidth="sm">
       <p>Demo entry point</p>
-      <MonthlyCalendar initialDate={currentDate} />
-      <WeeklyCalendar initialDate={currentDate} />
-      <TestTable />
 
-      <Table>
-        <WeekdayHeaderMUI />
-      </Table>
+      <MonthlyCalendar
+        initialDate={currentDate}
+        onExpand={date => setExpandedDate(date)}
+      />
 
-      <MonthlyCalendarMUI initialDate={currentDate} />
+      {expandedDate && (
+        <WeeklyCalendar
+          key={expandedDate.format('MM D')}
+          initialDate={expandedDate}
+        />
+      )}
     </Container>
   )
 }
-
-function TestTable() {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ border: '1px dashed orange' }} aria-label="Test table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Foo</TableCell>
-            <TableCell>Bar</TableCell>
-            <TableCell>Baz</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>Cell 1</TableCell>
-            <TableCell>Cell 2</TableCell>
-            <TableCell>Cell 3</TableCell>
-          </TableRow>
-
-          <TableRow>
-            <TableCell>Cell 4</TableCell>
-            <TableCell>Cell 5</TableCell>
-            <TableCell>Cell 6</TableCell>
-          </TableRow>
-        </TableBody>
-        {/*       
-
-      <TableRow>
-        <TableHead>H1</TableHead>
-        <TableCell>Cell 1</TableCell>
-        <TableCell>Cell 2</TableCell>
-      </TableRow>
-
-      <TableRow>
-        <TableHead>H2</TableHead>
-        <TableCell>Cell 3</TableCell>
-        <TableCell>Cell 4</TableCell>
-      </TableRow> */}
-      </Table>
-    </TableContainer>
-  )
-}
-
-function DayCard({ children }) {
-  return (
-    <Box
-      width="14.2%"
-      height="40px"
-      sx={{ backgroundColor: '#fff1', border: '1px solid #0004' }}
-    >
-      {children}
-    </Box>
-  )
-}
-
-// function
 
 function WeeklyCalendar({ initialDate }) {
   const [active, setActive] = useState(initialDate)
@@ -111,11 +61,17 @@ function WeeklyCalendar({ initialDate }) {
       d = d.add(1, 'day')
     }
 
-    return days.map(d => (
-      <DayCard key={d.format('MM D')}>
-        <Typography>{d.format('D')}</Typography>
-      </DayCard>
-    ))
+    return (
+      <TableBody>
+        <TableRow>
+          {days.map(d => (
+            <TableCell key={d.format('MM D')}>
+              <Typography>{d.format('D')}</Typography>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableBody>
+    )
   }, [active])
 
   log(`(${(Math.random() * 1000).toFixed()}) Rendering weekly calendar`)
@@ -138,8 +94,13 @@ function WeeklyCalendar({ initialDate }) {
             >
               Week of {active.startOf('week').format('MMMM D, YYYY')}
             </Typography>
-            <WeekdayHeader />
-            {calendarBody}
+
+            <TableContainer component={Paper}>
+              <Table>
+                <WeekdayHeader />
+                {calendarBody}
+              </Table>
+            </TableContainer>
           </Stack>
 
           <IconButton
@@ -154,13 +115,13 @@ function WeeklyCalendar({ initialDate }) {
   )
 }
 
-function WeekdayHeader() {
-  return ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(d => (
-    <DayCard key={d}>{d}</DayCard>
-  ))
-}
+const HoverableRow = styled(TableRow)(({ theme }) => ({
+  '&:hover': {
+    backgroundColor: theme.palette.action.hover,
+  },
+}))
 
-function WeekdayHeaderMUI() {
+function WeekdayHeader() {
   return (
     <TableHead>
       <TableRow>
@@ -172,88 +133,7 @@ function WeekdayHeaderMUI() {
   )
 }
 
-function MonthlyCalendar({ initialDate }) {
-  const [active, setActive] = useState(initialDate)
-  const month = active.format('MMMM')
-  const year = active.year()
-
-  const calendarBody = useMemo(() => {
-    log(`📆 (${(Math.random() * 1000).toFixed()}) memoizing month display`)
-    const days = []
-    const startOfMonth = active.startOf('month')
-    const endOfMonth = active.endOf('month')
-
-    let d = startOfMonth
-    let pad = d.day(0)
-
-    while (pad.isBefore(d)) {
-      days.push(pad)
-      pad = pad.add(1, 'day')
-    }
-
-    while (d.isBefore(endOfMonth)) {
-      days.push(d)
-      d = d.add(1, 'day')
-    }
-
-    while (d.day() !== 0) {
-      days.push(d)
-      d = d.add(1, 'day')
-    }
-
-    return days.map(d => (
-      <DayCard key={d.format('MM D')}>
-        <Typography
-          sx={{
-            opacity:
-              d.isBefore(startOfMonth) || d.isAfter(endOfMonth)
-                ? 0.2
-                : undefined,
-          }}
-        >
-          {d.format('D')}
-        </Typography>
-      </DayCard>
-    ))
-  }, [active])
-
-  log(`(${(Math.random() * 1000).toFixed()}) Rendering monthly calendar`)
-  return (
-    <Box>
-      <Paper elevation={1} sx={{ px: 2, py: 2 }}>
-        <Stack direction="row">
-          <IconButton
-            aria-label="previous month"
-            onClick={() => setActive(active.subtract(1, 'month'))}
-          >
-            <NavigateBeforeIcon />
-          </IconButton>
-
-          <Stack direction="row" flexWrap="wrap" sx={{ mt: 1, mb: 4 }}>
-            <Typography
-              variant="h4"
-              component="div"
-              sx={{ width: '100%', mb: 3 }}
-            >
-              {month} {year}
-            </Typography>
-            <WeekdayHeader />
-            {calendarBody}
-          </Stack>
-
-          <IconButton
-            aria-label="next month"
-            onClick={() => setActive(active.add(1, 'month'))}
-          >
-            <NavigateNextIcon />
-          </IconButton>
-        </Stack>
-      </Paper>
-    </Box>
-  )
-}
-
-function MonthlyCalendarMUI({ initialDate }) {
+function MonthlyCalendar({ initialDate, onExpand }) {
   const [active, setActive] = useState(initialDate)
   const month = active.format('MMMM')
   const year = active.year()
@@ -305,11 +185,18 @@ function MonthlyCalendarMUI({ initialDate }) {
         )
       }
 
-      body.push(<TableRow key={days[i].format('MM D')}>{week}</TableRow>)
+      body.push(
+        <HoverableRow
+          key={days[i].format('MM D')}
+          onClick={() => onExpand(days[i])}
+        >
+          {week}
+        </HoverableRow>
+      )
     }
 
     return <TableBody>{body}</TableBody>
-  }, [active])
+  }, [active, onExpand])
 
   log(`(${(Math.random() * 1000).toFixed()}) Rendering monthly calendar`)
   return (
@@ -332,11 +219,12 @@ function MonthlyCalendarMUI({ initialDate }) {
               {month} {year}
             </Typography>
 
-            <Table>
-              <WeekdayHeaderMUI />
-
-              {calendarBody}
-            </Table>
+            <TableContainer component={Paper}>
+              <Table>
+                <WeekdayHeader />
+                {calendarBody}
+              </Table>
+            </TableContainer>
           </Stack>
 
           <IconButton
