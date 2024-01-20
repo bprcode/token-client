412ca331874c7f63c3e5c9e140a673c15f64baef
bprcode <bprcode@gmail.com>
2024-01-20 07:31:26 -0800

    Style creation popups

--- src/calendar/CreationPicker.jsx
+++ src/calendar/CreationPicker.jsx
@@ -1,4 +1,12 @@
-import { Box, FormControl, InputLabel, MenuItem, Select, TextField, Typography, useTheme } from '@mui/material'
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  useTheme,
+} from '@mui/material'
 import { useState } from 'react'
 import { mockPalette, resolveColor, useEventStyles } from './calendarLogic.mjs'
 
@@ -26,8 +34,6 @@ const styledMenuProps = {
         borderBottom: '1px solid #0008',
         borderRight: '1px solid #0008',
         boxShadow: '2px 2px 8px #000209d0',
-
-
       },
       '& li:hover': {
         backgroundColor: 'rgb(64, 64, 64)',
@@ -66,118 +72,133 @@ export function CreationPicker() {
       }}
     >
       <span>
-      
-      <FormControl variant="standard"
-      sx={{
-        mr: rightMargin,
-        ml: '0.25rem',
-
-      }}>
-        <InputLabel id="event-type-label">Type</InputLabel>
-        <Select
-          labelId="event-type-label"
-          value={type}
-          onChange={e => setType(e.target.value)}
-          MenuProps={styledMenuProps}
-        >
-          {typeList.map(t => (
-            <MenuItem
-              key={t}
-              value={t}
-              sx={{
-                '&&': {
-                borderLeft: `0.5rem solid ${resolveColor(t)}`
-                },
-                minWidth: '120px',
-              }}
-            >
-              {t !== 'Default' ? t : 'Custom'}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-
-      {/* Show color icon, or selector for custom events. */}
-      {type !== 'Default' && <div
-        style={{
-          display: 'inline-block',
-          width: condensedIconWidth,
-          height: colorIconHeight,
-          borderRadius: '4px',
-          backgroundColor: `${resolveColor(type)}`,
-          verticalAlign: 'bottom',
-          marginBottom: '0.5rem',
-        }}
-      />}
-
-      {type === 'Default' && <><FormControl variant="standard" sx={{mr: rightMargin}}>
-        <InputLabel id="color-label">Color</InputLabel>
-        <Select
-          labelId="color-label"
-          value={selectedColor}
-          onChange={e => setSelectedColor(e.target.value)}
-          MenuProps={styledMenuProps}
+        <FormControl
+          variant="standard"
           sx={{
-            '& .palette-block': {
-              width: condensedIconWidth,
-            }
+            mr: rightMargin,
+            ml: '0.25rem',
           }}
         >
-          {palette.map((c,i) => (
-            <MenuItem
-              key={c}
-              value={c}
-              sx={{
-                '&&': { px: '0.5rem',
-                borderTopLeftRadius: i === 0 ? '8px' : '0',
-                borderTopRightRadius: i === 0 ? '8px' : '0',
-                borderBottomLeftRadius: i === palette.length-1 ? '8px' : '0',
-                borderBottomRightRadius: i === palette.length-1 ? '8px' : '0',
-                mb: 0,
-              },
-                
-              }}
-            >
-              <Box
-                className="palette-block"
+          <InputLabel id="event-type-label">Type</InputLabel>
+          <Select
+            labelId="event-type-label"
+            value={type}
+            onChange={e => setType(e.target.value)}
+            MenuProps={styledMenuProps}
+          >
+            {typeList.map(t => (
+              <MenuItem
+                key={t}
+                value={t}
                 sx={{
-                  backgroundColor: c,
-                  width: colorIconWidth,
-                  height: colorIconHeight,
-                  borderRadius: '2px',
-                  mx: '2px',
+                  '&&': {
+                    borderLeft: `0.5rem solid ${resolveColor(t)}`,
+                  },
+                  minWidth: '120px',
                 }}
-              />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+              >
+                {t !== 'Default' ? t : 'Custom'}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-      <TextField id="standard-basic" label="Title" variant="standard" 
-      defaultValue='Event'
-      sx={{
-        maxWidth: '14ch',
-      }}/>
+        {/* Show color icon if not creating a custom event. */}
+        {type !== 'Default' && (
+          <div
+            style={{
+              display: 'inline-block',
+              width: condensedIconWidth,
+              height: colorIconHeight,
+              borderRadius: '4px',
+              backgroundColor: `${resolveColor(type)}`,
+              verticalAlign: 'bottom',
+              marginBottom: '0.5rem',
+            }}
+          />
+        )}
 
-      </>}
-        </span>
-      {type !== 'Default' && <div style={{
-        display: 'flex',
-        fontSize: '0.75em',
-        flexGrow: 1,
-        height: '48px',
-        justifyContent: 'end',
-        alignItems: 'start',
-        marginRight: '0.75rem',
-        color: theme.palette.primary.light,
-        }}>
-          <div style={{
-            paddingBottom: '5px',
-            }}>
+        {type === 'Default' && (
+          <>
+            <FormControl variant="standard" sx={{ mr: rightMargin }}>
+              <InputLabel id="color-label">Color</InputLabel>
+              <Select
+                labelId="color-label"
+                value={selectedColor}
+                onChange={e => setSelectedColor(e.target.value)}
+                MenuProps={styledMenuProps}
+                sx={{
+                  '& .palette-block': {
+                    width: condensedIconWidth,
+                    transform: 'translateX(2px)',
+                  },
+                }}
+              >
+                {palette.map((c, i) => (
+                  <MenuItem
+                    key={c}
+                    value={c}
+                    sx={{
+                      '&&': {
+                        px: '0.5rem',
+                        borderTopLeftRadius: i === 0 ? '8px' : '0',
+                        borderTopRightRadius: i === 0 ? '8px' : '0',
+                        borderBottomLeftRadius:
+                          i === palette.length - 1 ? '8px' : '0',
+                        borderBottomRightRadius:
+                          i === palette.length - 1 ? '8px' : '0',
+                        mb: 0,
+                      },
+                    }}
+                  >
+                    <Box
+                      className="palette-block"
+                      sx={{
+                        backgroundColor: c,
+                        width: colorIconWidth,
+                        height: colorIconHeight,
+                        borderRadius: '2px',
+                      }}
+                    />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
 
-        Tap & drag to add
+            <TextField
+              id="standard-basic"
+              label="Title"
+              variant="standard"
+              defaultValue="Event"
+              sx={{
+                maxWidth: '14ch',
+              }}
+            />
+          </>
+        )}
+      </span>
+      {type !== 'Default' && (
+        <div
+          style={{
+            display: 'flex',
+            fontSize: '0.75em',
+            flexGrow: 1,
+            height: '48px',
+            justifyContent: 'end',
+            alignItems: 'start',
+            marginRight: '0.75rem',
+            color: theme.palette.primary.light,
+          }}
+        >
+          <div
+            style={{
+              paddingBottom: '5px',
+            }}
+          >
+            Tap & drag to add
           </div>
-        </div>}
+        </div>
+      )}
     </div>
   )
 }
