75feff310f65559aea8a13c6a34d5835f4e49585
bprcode <bprcode@gmail.com>
2024-04-10 11:47:49 -0700

    Experiment with reveal effect

--- src/landing/welcome.mjs
+++ src/landing/welcome.mjs
@@ -1,6 +1,37 @@
 console.log('welcome.mjs imported.')
 
 const watchedElement = document.querySelector('.globe-section')
+const mainSections = [...document.querySelectorAll('main section')].map(s => ({
+  element: s,
+  midline: s.offsetTop + s.getBoundingClientRect().height / 2,
+  bottom: s.offsetTop + s.getBoundingClientRect().height,
+  img: s.querySelector('img'),
+}))
+
+
+function overwriteRAF(callback) {
+  if (!overwriteRAF.callback) {
+    requestAnimationFrame(() => {
+      overwriteRAF.callback()
+      overwriteRAF.callback = null
+    })
+  }
+
+  overwriteRAF.callback = callback
+}
+
+function onScroll(event) {
+  overwriteRAF(() => {
+    const scrollBottom = window.scrollY + window.innerHeight
+
+    for (const s of mainSections) {
+      const ny = (s.bottom - scrollBottom) / window.innerHeight
+      console.log(s, ny)
+      s.img.style.opacity = (1 - ny)
+    }
+    
+  })
+}
 
 function loadImages() {
   const loadList = document.querySelectorAll('.lazy')
@@ -11,11 +42,11 @@ function loadImages() {
       console.log('setting i', 'to', i.dataset.src)
       i.src = i.dataset.src
       i.classList.add('seen')
-    }, 3000)
+    }, 1000)
   }
 }
 
-function watch(entries, observer) {
+function watchMain(entries, observer) {
   for (const e of entries) {
     console.log(e, 'is intersecting?', e.isIntersecting)
     if (e.isIntersecting) {
@@ -27,15 +58,33 @@ function watch(entries, observer) {
   }
 }
 
-const observer = new IntersectionObserver(watch, {
+// function watchImages(entries, observer) {
+//   for (const e of entries) {
+    
+//     console.log(e.intersectionRatio, e.target)
+//     // console.log(i, 'has intersection ratio', i.intersectionRatio)
+
+//   }
+// }
+
+const loadObserver = new IntersectionObserver(watchMain, {
   threshold: 0.01,
 })
 
+// const imageObserver = new IntersectionObserver(watchImages)
+
+// for(const e of document.querySelectorAll('main section')) {
+//   console.log('checking e:',e.querySelector('img'))
+//   imageObserver.observe(e.querySelector('img'))
+// }
+
 if (window.scrollY === 0) {
-  observer.observe(watchedElement)
+  loadObserver.observe(watchedElement)
 } else {
   console.log('immediately load images')
   loadImages()
 }
 
 document.querySelector('h1').innerText = '😴 scrollY=' + window.scrollY
+
+window.addEventListener('scroll', onScroll)
