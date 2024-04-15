16d0dbc560f93092414b6941ceb91bef8e9849c3
bprcode <bprcode@gmail.com>
2024-04-15 10:16:00 -0700

    Prune old code

--- src/SpinOrText.jsx
+++
@@ -1,5 +0,0 @@
-import { CircularProgress } from '@mui/material'
-
-export default function SpinOrText({ spin, text }) {
-  return spin ? <CircularProgress size="1.5rem" /> : text
-}
--- src/calendar-test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Cal from './Cal.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Cal />
-  </React.StrictMode>,
-)
