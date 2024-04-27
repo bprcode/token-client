878e9e1aa177b4976be10bec414a0ab012872381
bprcode <bprcode@gmail.com>
2024-04-27 16:21:30 -0700

    Establish production log switcher

--- src/log.mjs
+++ src/log.mjs
@@ -1,23 +1,49 @@
 import { debounce } from './debounce'
 
-const maxBufferSize = 30
+const maxBufferSize = 50
 const logBuffer = []
-const logImmediate = console.log.bind(console)
 
-const logDelayed = (...stuff) => {
-  logBuffer.push(stuff)
-  while(logBuffer.length > maxBufferSize) {
-    logBuffer.shift()
+function dumpLog() {
+  while (logBuffer.length) {
+    console.log(...logBuffer.shift())
   }
-
-  debounce('log dump', () => {
-    while(logBuffer.length) {
-      logImmediate(...logBuffer.shift())
-    }
-  }, 500)
 }
 
-const log = import.meta.env.DEV ? console.log.bind(console) : () => {}
+function logEntry(stuff) {
+  logBuffer.push(stuff)
+  while (logBuffer.length > maxBufferSize) {
+    logBuffer.shift()
+  }
+}
 
-export default logDelayed
- 
+function logDelayed(...stuff) {
+  logEntry(stuff)
+  debounce('log dump', dumpLog, 200)
+}
+
+function logInternal(...stuff) {
+  logEntry(stuff)
+}
+
+const logReference = {
+  current: import.meta.env.DEV ? logDelayed : logInternal,
+}
+
+function log(...stuff) {
+  logReference.current(...stuff)
+}
+
+window.enableLogs = function () {
+  console.log(
+    `%cLogging enabled. Most recent ${logBuffer.length} messages:`,
+    'color:#0af'
+  )
+  dumpLog()
+  logReference.current = logDelayed
+}
+
+if (logReference.current === logInternal) {
+  console.log('🔹%cLogging available. Run enableLogs() to view.', 'color:#0af')
+}
+
+export default log
